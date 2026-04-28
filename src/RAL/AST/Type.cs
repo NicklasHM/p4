f123085a9727007f4fc71e06e2c69f2054f041e0
Jacob580234 <jsand24@student.aau.dk>
2026-04-28 12:41:35 +0200

    Tiny structure

--- src/RAL/AST/Type.cs
+++ src/RAL/AST/Type.cs
@@ -1,1 +1,43 @@
-abstract class Type{};
+namespace RAL.AST;
+
+public interface Type {}
+
+public sealed class BoolT : Type {
+    public static readonly BoolT Instance = new();
+    private BoolT() {}
+}
+
+public sealed class NumberT : Type {
+    public static readonly NumberT Instance = new();
+    private NumberT() {}
+}
+
+public sealed class StringT : Type {
+    public static readonly StringT Instance = new();
+    private StringT() {}
+}
+
+public sealed class ResourceT : Type {
+    public static readonly ResourceT Instance = new();
+    private ResourceT() {}
+}
+
+public sealed class ReservationT : Type {
+    public static readonly ReservationT Instance = new();
+    private ReservationT() {}
+}
+
+public sealed class DurationT : Type {
+    public static readonly DurationT Instance = new();
+    private DurationT() {}
+}
+
+public sealed class DateTimeT : Type {
+    public static readonly DateTimeT Instance = new();
+    private DateTimeT() {}
+}
+
+public sealed class CategoryT : Type {
+    public static readonly CategoryT Instance = new();
+    private CategoryT() {}
+}
---
+++ src/RAL/TypeChecker/EnvV.cs
@@ -0,0 +1,15 @@
+namespace RAL.TC;
+using RAL.AST;
+
+public class EnvV {
+    
+    private readonly Dictionary<string, Type> H = new();
+
+    public void bind(string var, Type type) {
+        if(H.containsKey(var)) {
+            
+        }
+    }
+
+
+}
