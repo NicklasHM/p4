745660955dfd670f1963398c382570bf105d8cb6
Jacob580234 <jsand24@student.aau.dk>
2026-04-28 16:57:34 +0200

    Binary operations for type checker progressed; should be almost done. (Formatting and structure is terrible, subject to change)

--- src/RAL/TypeChecker/EnvV.cs
+++ src/RAL/TypeChecker/EnvV.cs
@@ -2,14 +2,47 @@ namespace RAL.TC;
 using RAL.AST;
 
 public class EnvV {
-    
-    private readonly Dictionary<string, Type> H = new();
+    private readonly Dictionary<string, Type> E = new();
+    private readonly EnvV? parent;
 
-    public void bind(string var, Type type) {
-        if(H.containsKey(var)) {
-            
+    public EnvV(EnvV? parent = null) {
+        this.parent = parent;
+    }
+
+    public EnvV NewScope() {
+        return new EnvV(this);
+    }
+
+    public void Bind(string var, Type type) {
+        if (E.ContainsKey(var)) {
+            throw new Exception($"Variable '{var}' already declared in current scope.");
+        }
+
+        E[var] = type;
+    }
+
+
+    public void ChangeCategory(string var, Type type) {
+        if (E.ContainsKey(var)) {
+            E[var] = type;
+        } else {
+            throw new Exception($"Use of undeclared variable: '{var}'.");
         }
     }
 
 
+    public Type Lookup(string var) {
+        if (E.TryGetValue(var, out var type)) {
+            return type;
+        } else if (parent != null) {
+            return parent.Lookup(var);
+        } else {
+            throw new Exception($"Use of undeclared variable: '{var}'.");
+        }
+    }
+
+
+    public bool IsLocal(string var) {
+        return E.ContainsKey(var);
+    }
 }
--- src/RAL/TypeChecker/TypeChecker.cs
+++ src/RAL/TypeChecker/TypeChecker.cs
@@ -2,7 +2,10 @@ using RAL.AST;
 namespace RAL.TC;
 
 class TypeChecker {
-    private Type ExpType(Exp exp) {
+
+    public List<string> errors = new();
+
+    private Type ExpType(Exp exp, EnvV env) {
         switch(exp) {
             case BoolV:        return BoolT;
             case StringV:      return StringT;
@@ -11,13 +14,71 @@ class TypeChecker {
             case DateTimeV:    return DateTimeT;
             case ResourceV:    return ResourceT;
             case ReservationV: return ReservationT;
-            // case RefV r: environment lookup to check type: tuple for template calls ?
-            default: {
-
-
-
-
+            case RefV r:       return env.Lookup(r.VariableId); // return the type of the variable
+            case BinaryOperation: { // turn into function to avoid confusing nesting?
+                Type left  = ExpType(exp.LeftExpression,  env);
+                Type right = ExpType(exp.RightExpression, env);
+                switch(exp.Operator) {
+                     case BinaryOperator.ADD:
+                     case BinaryOperator.SUB: {
+                        switch(left, right) {
+                            case (NumberT, NumberT): break; // num + num
+                            case (DateTimeT, DurationT): break; // dt + dur
+                            default: errors.Add($"Line {exp.LeftExpression.LineNumber}: Operands '{left}' and '{right}' are incompatible for '{(exp.Operator == BinaryOperator.ADD ? '+' : '-')}'."); break;            
+                        }  
+                    }
+                    case BinaryOperator.MUL:
+                    case BinaryOperator.DIV: {
+                         switch(left, right) {
+                            case (NumberT, NumberT): break;
+                            default: errors.Add($"Line {exp.LeftExpression.LineNumber}: Operands '{left}' and '{right}' are incompatible for '{(exp.Operator == BinaryOperator.MUL ? '*' : '/')}'."); break;                      
+                        }       
+                    }
+                    case BinaryOperator.LT:
+                    case BinaryOperator.GT:
+                    case BinaryOperator.LTEQ:
+                    case BinaryOperator.GTEQ: {
+                        switch(left, right) {
+                            case (NumberT, NumberT): break;
+                            default: errors.Add($"Line {exp.LeftExpression.LineNumber}: Operands '{left}' and '{right}' are incompatible for '{(exp.Operator == BinaryOperator.LT ? "<" : exp.Operator == BinaryOperator.GT ? ">" : exp.Operator == BinaryOperator.LTEQ ? "<=" : ">=")}'."); break;           
+                        }            
+                    }
+                    case BinaryOperator.EQ:
+                    case BinaryOperator.NEQ: {
+                         switch(left, right) {
+                            case(BoolT, BoolT): break;
+                            case(NumberT, NumberT): break;
+                            default: errors.Add($"Line {exp.LeftExpression.LineNumber}: Operands '{left}' and '{right}' are incompatible for '{(exp.Operator == BinaryOperator.EQ ? "==" : "!=")}'."); break;                                  
+                        }       
+                    }
+                    case BinaryOperator.OR: {
+                        switch(left, right) {
+                             case(BoolT, BoolT): break;
+                             case(ReservationT, ReservationT): break;
+                             default: errors.Add($"Line {exp.LeftExpression.LineNumber}: Operands '{left}' and '{right}' are incompatible for 'or'."); break;                                           
+                        }            
+                    }
+                    case BinaryOperator.AND: {
+                        switch(left, right) {
+                            case(ResourceT, ResourceT): break;
+                            case(ReservationT, ReservationT): break;
+                            case(BoolT, BoolT): break;
+                            default: errors.Add($"Line {exp.LeftExpression.LineNumber}: Operands '{left}' and '{right}' are incompatible for 'and'."); break;                                          
+                        }            
+                    }
+                    case BinaryOperator.SEQ: {
+                        switch(left, right) {
+                            case(ReservationT, ReservationT): break;
+                            default: errors.Add($"Line {exp.LeftExpression.LineNumber}: Operands '{left}' and '{right}' are incompatible for 'seq'."); break;                                              
+                        } 
+                    }
+                }
             }
+            case UnaryOperation: {
+                    
+            }
+
+            default: throw new Exception("Unknown type."); // should never happen
         }
     }
 
@@ -31,11 +92,8 @@ class TypeChecker {
 
     }
 
-
-
-
-
 }
+
 
 
 /*
