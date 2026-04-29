97ccc0624f66caf23dc0ca6d07f8b1e04d954900
Abtin Rezvani <151622443+abtrez@users.noreply.github.com>
2026-04-29 19:56:14 +0200

    domain done, time just needs string extrapolation
    type is not included, as it may be on type checker branch. Duration and time constructors need to be supplied correct arguments.

--- src/RAL/AST/Exp.cs
+++ src/RAL/AST/Exp.cs
@@ -4,17 +4,24 @@ abstract record class Exp(int LineNumber);
 
 record class BinaryOperation(int LineNumber, Exp LeftExpression, BinaryOperator Operator, Exp RightExpression ) : Exp(LineNumber);
 
-record class UnaryOperation(int LineNumber, UnaryOperator? Operator, Exp Expression ): Exp(LineNumber);
+record class UnaryOperation(int LineNumber, UnaryOperator Operator, Exp Expression ): Exp(LineNumber);
 
 //See the dictionary key type in /Interpreter/EnvV.cs for the choice of string type Identifier.
 record class Assignment(int LineNumber, string VariableId, string? PropertyId, Exp Value) : Exp(LineNumber);
 
+record class Reserve(int LineNumber, QueryData Query) : Exp(LineNumber);
+
+/*                                     might be id, might be a reserve expression */
+record class Reschedule(int LineNumber, Exp Reservation, TimeSpec NewTimeInterval) : Exp(LineNumber);
+
 record class BoolV(int LineNumber, bool Value) : Exp(LineNumber);
 
 record class StringV(int LineNumber, string Value) : Exp(LineNumber);
+
 record class Reference(int LineNumber, string VariableId, string? PropertyId) : Exp(LineNumber);
 
 record class DateTimeV(int LineNumber, DateTime Value) : Exp(LineNumber);
+
 record class DurationV(int LineNumber, TimeSpan Value) : Exp(LineNumber);
 
 enum BinaryOperator
@@ -30,11 +37,3 @@ enum UnaryOperator
 {
     NOT//, NEG
 }
-
-/* Work in progress */
-record class AvailabilityExp(int LineNumber ) : Exp(LineNumber);
-
-record class QueryExp(int LineNumber, ResourceExp Resource, TimeExp Time, Exp? ExpCondition) : Exp(LineNumber);
-
-record class TimeExp(int LineNumber, DateTime StartDate, DateTime? ToDate, Duration? ForTime) : Exp(LineNumber);
-record class ResourceExp(int LineNumber, Exp? AdditiveExp, string? MoreOf, string Identifier, ResourceExp? AndResource) : Exp(LineNumber);
